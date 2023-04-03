c6e7de8a08f0536677ab1fd265f63ce21670d722
Muhammad Shaheer <shaheer.chry@gmail.com>
2023-04-03 12:43:18 +0500

    Other Report Hazard Form corrected

--- client/src/components/user/hazardReportForm.js
+++ client/src/components/user/hazardReportForm.js
@@ -1,21 +1,16 @@
-import { useState, useRef } from 'react';
+import { useState } from "react";
 
 function HazardReportForm(props) {
-  const [desc, setDesc] = useState('');
-  const [department, setDepartment] = useState('');
-  const [area, setArea] = useState('');
-  const [resp, setResp] = useState('');
-  const [reportDate, setReportDate] = useState('');
-  const [reportTime, setReportTime] = useState('');
-  const [photo, setPhoto] = useState('');
-  const inputRef = useRef(null);
+  const [site, setSite] = useState("");
+  const [department, setDepartment] = useState("");
+  const [area, setArea] = useState("");
+  const [userType, setUserType] = useState("");
+  const [reportedStatus, setReportedStatus] = useState("");
+  const [reportStartDate, setReportStartDate] = useState("");
+  const [reportEndDate, setReportEndDate] = useState("");
 
-  const handleClick = () => {
-    inputRef.current.click();
-  };
-
-  const handleDescChange = (event) => {
-    setDesc(event.target.value);
+  const handleSiteChange = (event) => {
+    setSite(event.target.value);
   };
 
   const handleDepartmentChange = (event) => {
@@ -26,42 +21,55 @@ function HazardReportForm(props) {
     setArea(event.target.value);
   };
 
-  const handleRespChange = (event) => {
-    setResp(event.target.value);
+  const handleUserTypeChange = (event) => {
+    setUserType(event.target.value);
   };
 
-  const handleReportDateChange = (event) => {
-    setReportDate(event.target.value);
+  const handleReportedStatusChange = (event) => {
+    setReportedStatus(event.target.value);
   };
 
-  const handleReportTimeChange = (event) => {
-    setReportTime(event.target.value);
+  const handleReportStartDateChange = (event) => {
+    setReportStartDate(event.target.value);
   };
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setPhoto(file);
+  const handleReportEndDateChange = (event) => {
+    setReportEndDate(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Call a function from the parent component to handle form submission
-    const obj = {
+    const obj = ({
+      site,
       department,
       area,
-      desc,
-      reportDate,
-      reportTime,
-      photo,
-      resp,
-    };
-    console.log('Form submitted');
+      userType,
+      reportedStatus,
+      reportStartDate,
+      reportEndDate,
+    });
+    console.log("Form submitted");
     console.log(obj);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Hazard Reporting</h1>
+
+<h1>Hazard Reports</h1>
+
+      <div>
+        <label htmlFor="site">Site:</label>
+        <input
+          type="text"
+          id="site"
+          name="site"
+          value={site}
+          onChange={handleSiteChange}
+          required
+          validationMessage="Required"
+        />
+      </div>
       <div>
         <label htmlFor="department">Department:</label>
         <input
@@ -71,6 +79,7 @@ function HazardReportForm(props) {
           value={department}
           onChange={handleDepartmentChange}
           required
+          validationMessage="Required"
         />
       </div>
       <div>
@@ -82,70 +91,58 @@ function HazardReportForm(props) {
           value={area}
           onChange={handleAreaChange}
           required
+          validationMessage="Required"
         />
       </div>
       <div>
-        <label htmlFor="site">Description:</label>
+        <label htmlFor="userType">User Type:</label>
         <input
           type="text"
-          id="site"
-          name="site"
-          value={desc}
-          onChange={handleDescChange}
+          id="userType"
+          name="userType"
+          value={userType}
+          onChange={handleUserTypeChange}
           required
+          validationMessage="Required"
         />
       </div>
       <div>
-        <label htmlFor="reportDate">Report Date:</label>
+        <label htmlFor="reportedStatus">Reported Status:</label>
+        <input
+          type="text"
+          id="reportedStatus"
+          name="reportedStatus"
+          value={reportedStatus}
+          onChange={handleReportedStatusChange}
+          required
+          validationMessage="Required"
+        />
+      </div>
+      <div>
+        <label htmlFor="reportStartDate">Report Start Date:</label>
         <input
           type="date"
-          id="reportDate"
-          name="reporttDate"
-          value={reportDate}
-          onChange={handleReportDateChange}
-          max={new Date().toISOString().split('T')[0]}
-          validationMessage="Report date cannot be in the future"
+          id="reportStartDate"
+          name="reportStartDate"
+          value={reportStartDate}
+          onChange={handleReportStartDateChange}
           required
+          validationMessage="Required"
         />
       </div>
       <div>
-        <label htmlFor="reportTime">Report Time</label>
+        <label htmlFor="reportEndDate">Report End Date:</label>
         <input
-          type="time"
-          id="reportTime"
-          name="reportTime"
-          value={reportTime}
-          onChange={handleReportTimeChange}
+          type="date"
+          id="reportEndDate"
+          name="reportEndDate"
+          value={reportEndDate}
+          onChange={handleReportEndDateChange}
           required
+          validationMessage="Required"
         />
       </div>
-      <div>
-        <button onClick={handleClick} type="button">
-          Upload photo
-        </button>
-        <input
-          ref={inputRef}
-          type="file"
-          accept="image/*"
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="resp">Responsibilities:</label>
-        <input
-          type="text"
-          id="resp"
-          name="resp"
-          value={resp}
-          onChange={handleRespChange}
-          required
-        />
-      </div>
-      <button onClick={handleSubmit} type="submit">
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
