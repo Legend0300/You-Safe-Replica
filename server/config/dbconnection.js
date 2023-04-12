c5bdcb1e736d01b7bc11512907613d94f49ec71e
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-04-13 00:11:12 +0500

    added ab connection

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -1,27 +1,9 @@
-// const { default: mongoose } = require("mongoose");
-// const dotenv = require('dotenv').config();
-
-// connect = async () => {
-//     try {
-//         await mongoose.connect(process.env.DB_KEY, {
-//         });
-//         console.log('Connected to MongoDB');
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// module.exports = connect;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// Replace the following with your Atlas connection string
+const { default: mongoose } = require("mongoose");
+const dotenv = require('dotenv').config();
 
 const connect = async () => {
     try {
-        await MongoClient.connect(process.env.DB_KEY, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverApi: ServerApiVersion.v1
+        await mongoose.connect(process.env.DB_KEY, {
         });
         console.log('Connected to MongoDB');
     } catch (error) {
@@ -30,6 +12,24 @@ const connect = async () => {
 }
 
 module.exports = connect;
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// // Replace the following with your Atlas connection string
+
+// const connect = async () => {
+//     try {
+//         await MongoClient.connect(process.env.DB_KEY, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             serverApi: ServerApiVersion.v1
+//         });
+//         console.log('Connected to MongoDB');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// module.exports = connect;
 
 // const uri = "mongodb+srv://minininja:RG7HHC5RTqCY5s8v@unicluster.pofs6l8.mongodb.net/?retryWrites=true&w=majority";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
