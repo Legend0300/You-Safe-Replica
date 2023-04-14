8ae98aff48e663249209d944d8dda4273cbcf661
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-04-14 20:42:10 +0500

    testing changes

--- server/middleware/managerAuth.js
+++ server/middleware/managerAuth.js
@@ -2,23 +2,23 @@ express = require('express');
 const jwt = require('jsonwebtoken');
 
 
-const validateToken = ( async (req, res, next) => {
-    // const authHeader = req.headers['authorization'] || req.headers['x-access-token'];
-    // const token = authHeader && authHeader.split(' ')[1];
-    const token = req.cookies.managerjwt;
+const validateToken = (req, res, next) => {
+  const token = req.cookies.userjwt;
 
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided'});
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (decodedToken.role !== 'user') {
+      return res.status(403).json({ message: 'Unauthorized' });
     }
-  
-    try {
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      req.manager = decodedToken.manager;
-      next();   
-    } catch (error) {
-      res.status(401).json({ message: 'Invalid token'});
-    }
-  
-})
+    req.user = decodedToken.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
 
 module.exports = validateToken;
--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const validateToken = ( async (req, res, next) => {
     // const authHeader = req.headers['authorization'] || req.headers['x-access-token'];
     // const token = authHeader && authHeader.split(' ')[1];
-    const token = req.cookies.userjwt;
+    const token = req.cookies.managerjwt;
 
     if (!token) {
       return res.status(401).json({ message: 'No token provided'});
@@ -13,7 +13,7 @@ const validateToken = ( async (req, res, next) => {
   
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decodedToken.user;
+      req.manager = decodedToken.manager;
       next();   
     } catch (error) {
       res.status(401).json({ message: 'Invalid token'});
