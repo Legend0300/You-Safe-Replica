c76d4bdc39094e87b2ab94ee05d478eb580af239
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-04-24 03:30:11 +0500

    Alt     1 file changed, 1 insertion(+), 1 deletion(-)

--- client/src/components/user/hazardReportForm.js
+++ client/src/components/user/hazardReportForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const HazardReportingForm = () => {
   const [reportedStatus, setReportedStatus] = useState('');
