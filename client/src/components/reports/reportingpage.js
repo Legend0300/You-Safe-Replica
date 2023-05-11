9379d9de923a3e774e597db3e830fbc1876c3021
26holiday <your-github-email@example.com>
2023-05-11 18:49:39 +0500

    created the reporting page

--- client/src/components/reports/reportingpage.js
+++ client/src/components/reports/reportingpage.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const ReportingPage = () => {
   const [reports, setReports] = useState([]);
+
+  // fetch reports from API
+  useEffect(() => {
+    const fetchReports = async () => {
+      const response = await fetch('http://localhost:4000/api/reports');
+      // const data = await response.json();
+      setReports(response.json);
+    };
+
+    fetchReports();
+  }, []);
 
   const submittedReports = reports.filter(report => report.status === 'submitted');
   const offlinePendingReports = reports.filter(report => report.status === 'offline_pending');
@@ -10,34 +21,24 @@ const ReportingPage = () => {
   return (
     <div>
       <h1>My Reporting</h1>
+
       <p>Total Submitted Reports: {submittedReports.length}</p>
       <p>Offline Pending Reports: {offlinePendingReports.length}</p>
+
       <h2>Submitted Reports</h2>
       <ul>
-        {submittedReports.map(report => (
-          <li key={report.id}>
-            <img src={report.picture} alt="Report" />
-            <p>Status: {report.status}</p>
-            <p>Type: {report.type}</p>
-            <p>Assigned to: {report.assignedTo}</p>
-            <p>Due Date: {report.dueDate}</p>
+        {reports.map(({ id, picture, status, type, assignedTo, dueDate }) => (
+          <li key={id}>
+            <img src={picture} alt="Report" />
+            <p>Status: {status}</p>
+            <p>Type: {type}</p>
+            <p>Assigned to: {assignedTo}</p>
+            <p>Due Date: {dueDate}</p>
             <button>View Details</button>
           </li>
         ))}
       </ul>
-      <h2>Offline Pending Reports</h2>
-      <ul>
-        {offlinePendingReports.map(report => (
-          <li key={report.id}>
-            <img src={report.picture} alt="Report" />
-            <p>Status: {report.status}</p>
-            <p>Type: {report.type}</p>
-            <p>Assigned to: {report.assignedTo}</p>
-            <p>Due Date: {report.dueDate}</p>
-            <button>View Details</button>
-          </li>
-        ))}
-      </ul>
+
       <Link to="/newreport">+</Link>
     </div>
   );
