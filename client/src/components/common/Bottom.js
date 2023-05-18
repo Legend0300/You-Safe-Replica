8af127c3db5d718d114a3907e277291c3fd165cb
Muhammad Shaheer <shaheer.chry@gmail.com>
2023-05-19 02:46:46 +0500

    Done

--- client/src/components/common/Bottom.js
+++ client/src/components/common/Bottom.js
@@ -4,32 +4,26 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PersonIcon from '@mui/icons-material/Person';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
-
   return (
     <Box sx={{ width: '100%', display: { xs: 'block', md: 'none' } }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <Link to="/reports" >
+      <BottomNavigation showLabels sx={{display:'flex', justifyContent: 'space-evenly', justifyItems: 'center'}}>
+        <Link to="/reports">
           <BottomNavigationAction
+            showLabel="true"
             label="My Reportings"
             icon={<CalendarMonthIcon />}
           />
-          <Typography>My Reports</Typography>
         </Link>
 
-        <Link to="/home" >
-          <BottomNavigationAction label="My Profile" icon={<PersonIcon />} />
-          <Typography>My Reports</Typography>
+        <Link to="/home">
+          <BottomNavigationAction
+            showLabel="true"
+            label="My Profile"
+            icon={<PersonIcon />}
+          />
         </Link>
       </BottomNavigation>
     </Box>
